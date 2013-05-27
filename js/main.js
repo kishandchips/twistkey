56ce62da18dc0016974581c4dbf1032922981173
franszo <franszo.design@gmail.com>
2013-05-27 14:21:16 +0100

    IE fallbacks, rotate script & png backgrounds  + fixed scroll to anchor script
    Signed-off-by: franszo <franszo.design@gmail.com>

--- js/main.js
+++ js/main.js
@@ -76,13 +76,13 @@
 				return this.each(function(){
 					var caller = this
 					$(caller).click(function (event) {	
-						event.preventDefault()
+						event.preventDefault();
 						var locationHref = window.location.href
-						var elementClick = $(caller).attr("href")
-						
-						var destination = $(elementClick).offset().top;
+						var elementClick = $(caller).attr("href")			
+						var destination = $(elementClick).offset().top -120;
+
 						$("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination}, settings.speed, function() {
-							window.location.hash = elementClick
+							// window.location.hash = elementClick;
 						});
 					  	return false;
 					})
